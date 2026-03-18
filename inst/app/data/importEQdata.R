49f8e9f0e7f1923263f839e5c1996e6953940098
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-03-18 06:44:31 -0400

    Update importEQdata.R

--- inst/app/data/importEQdata.R
+++ inst/app/data/importEQdata.R
@@ -223,6 +223,6 @@ save(filt.df, states_regions, addparameters_filter_poll, addparameters_filter_pg
 
 save(update.tmdls, orig.distinct.n, orig.dups.n, orig.dups.removed, orig.n,
      orig.noauid.n, orig.nopoll.n, orig.noauidpoll.n, max.dups, min.dups, filt.df.n,
-     file = "app/www/RMD_data.RData")
+     file = "inst/app/www/RMD_data.RData")
 }
 
