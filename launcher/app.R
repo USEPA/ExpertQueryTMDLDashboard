b3a38973533d6115e162e6f867146126a5948ca1
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-05-27 13:51:47 -0400

    Update app.R

--- launcher/app.R
+++ launcher/app.R
@@ -1,17 +1,19 @@
 local({
-  # Package mode?
-  if (nzchar(system.file(package = "TMDLDash"))) {
+  # If the package is installed, run from the installed app dir (package mode)
+  pkg_app <- system.file("app", package = "TMDLDash")
+  if (nzchar(pkg_app) && dir.exists(pkg_app)) {
     message("Running in PACKAGE mode")
-    source(system.file("app", "app.R", package = "TMDLDash"), local = TRUE)$value
+    # Source app.R and temporarily setwd() to that directory so relative paths work
+    source(file.path(pkg_app, "app.R"), local = TRUE, chdir = TRUE)$value
   } else {
     message("Running in PLAIN SHINY mode")
-    # Make helpers in R/ available in plain mode
+    # Make helpers in R/ available in plain mode (if you use files under R/)
     rdir <- file.path(getwd(), "R")
     if (dir.exists(rdir)) {
       rfiles <- list.files(rdir, pattern = "\\.[Rr]$", full.names = TRUE)
       lapply(rfiles, sys.source, envir = environment())
     }
-    # Run the app directly from the repo
-    source(file.path("inst", "app", "app.R"), local = TRUE)$value
+    # Source app from inst/app and temporarily setwd() there so relative paths work
+    source(file.path("inst", "app", "app.R"), local = TRUE, chdir = TRUE)$value
   }
 })
