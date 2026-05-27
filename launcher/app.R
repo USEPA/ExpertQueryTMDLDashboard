c67fb9d14158ef9075a6f94fcdf5195b00152661
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-05-27 13:32:41 -0400

    potential bug fixes

--- launcher/app.R
+++ launcher/app.R
@@ -1,13 +1,17 @@
-library(TMDLDash)
-
-message("Launcher starting at: ", Sys.time())
-app <- tryCatch(
-  TMDLDash::run_app(),
-  error = function(e) {
-    message("run_app() failed: ", conditionMessage(e))
-    traceback(2)
-    stop(e)
+local({
+  # Package mode?
+  if (nzchar(system.file(package = "TMDLDash"))) {
+    message("Running in PACKAGE mode")
+    source(system.file("app", "app.R", package = "TMDLDash"), local = TRUE)$value
+  } else {
+    message("Running in PLAIN SHINY mode")
+    # Make helpers in R/ available in plain mode
+    rdir <- file.path(getwd(), "R")
+    if (dir.exists(rdir)) {
+      rfiles <- list.files(rdir, pattern = "\\.[Rr]$", full.names = TRUE)
+      lapply(rfiles, sys.source, envir = environment())
+    }
+    # Run the app directly from the repo
+    source(file.path("inst", "app", "app.R"), local = TRUE)$value
   }
-)
-message("Launcher handing app to Shiny at: ", Sys.time())
-app
+})
