575e9478ab398adbe85a332bfe2cc85924a05fa3
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-07-30 17:30:01 -0400

    update deployment

--- launcher/scripts/importEQdata.R
+++ launcher/scripts/importEQdata.R
@@ -216,12 +216,16 @@ if(check.api == 200) {
   
   # create .RData files
   
-  save(filt.df, states_regions, addparameters_filter_poll, addparameters_filter_pg,
-       pollutants_groups, parameters, max_year, years_list, categories, update.tmdls,
-       act_agencies,
-       file = "launcher/extdata/EQ_data.RData")
+  save(
+    filt.df, states_regions, addparameters_filter_poll, addparameters_filter_pg,
+    pollutants_groups, parameters, max_year, years_list, categories, update.tmdls,
+    act_agencies,
+    file = file.path("launcher", "data", "EQ_data.RData")
+  )
   
-  save(update.tmdls, orig.distinct.n, orig.dups.n, orig.dups.removed, orig.n,
-       orig.noauid.n, orig.nopoll.n, orig.noauidpoll.n, max.dups, min.dups, filt.df.n,
-       file = "launcher/www/RMD_data.RData")
+  save(
+    update.tmdls, orig.distinct.n, orig.dups.n, orig.dups.removed, orig.n,
+    orig.noauid.n, orig.nopoll.n, orig.noauidpoll.n, max.dups, min.dups, filt.df.n,
+    file = file.path("launcher", "data", "RMD_data.RData")
+  )
 }
