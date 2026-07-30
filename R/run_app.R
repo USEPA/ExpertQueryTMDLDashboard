5ae5c64c0a5d0da7a233e81944294cd13c6fe7e8
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-07-30 13:46:49 -0400

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,27 +1,10 @@
 #' Launch the TMDL Dashboard Shiny app
 #' @export
 run_app <- function() {
-  # Preferred development location: source tree launcher/
-  candidates <- c(
-    file.path(getwd(), "launcher"),
-    system.file("launcher", package = "TMDLDash")
-  )
+  app_dir <- normalizePath(file.path(getwd(), "launcher"), winslash = "/", mustWork = TRUE)
   
-  app_dir <- candidates[dir.exists(candidates)][1]
-  
-  if (is.na(app_dir) || !nzchar(app_dir)) {
-    stop(
-      "Could not find the app directory.\n",
-      "Checked:\n  - ", shQuote(file.path(getwd(), "launcher")), "\n",
-      "  - ", shQuote(system.file("launcher", package = "TMDLDash")), "\n",
-      call. = FALSE
-    )
-  }
-  
-  # Make the path discoverable by the app’s code
   options(TMDLDash.app_dir = app_dir)
   message("startup: using app_dir = ", app_dir)
   
-  # Return a Shiny app object from that directory
   shiny::shinyAppDir(app_dir)
 }
